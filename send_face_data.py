009a4adb305611ce0a2399742a3cd0aa9a05ef05
fangzaizai <anmeifang111@sina.com>
2017-10-17 23:53:13 +0800

    multi add
    so

--- send_face_data.py
+++ send_face_data.py
@@ -1,6 +1,7 @@
 # -*- coding:utf-8 -*-
 import websocket
 import json
+from datetime import datetime
 
 def send_face_data():
 	ws = websocket.create_connection("ws://192.168.9.208:19101/FACE-DETECT-WS/websck")
@@ -24,6 +25,34 @@ def send_face_data():
 	ws.send(json.dumps(data))
 	result = ws.recv()
 	print result
+	ws.close()
+
+def send_face_data(data, time):
+	ws = websocket.create_connection("ws://192.168.9.208:19101/FACE-DETECT-WS/websck")
+	face_unid_unique=111111111
+	face_refid_unique=111111111
+	picname=1
+	for i in xrange(1,time):		
+		data={
+		"type":"request",
+		"id":face_unid_unique+1,
+		"command":"send_face_events",
+		"params":[
+			{
+				"face_refid":face_refid_unique+1,
+				"event_refid":event_refid+1,
+				"event_dt":datetime.now(),
+				"pic_url":"http://192.168.9.208:7788//LINDASceneAlarm/20171017/16/" + (picname+1) + ".jpg",
+				"accord_event_refid":"DF452E44-B458-446D-9273-35472618DE73}-20171011160011826",
+				"accord_similarity":90,
+				"camera_refid":"0557fc08-565d-449d-82b9-22ad08e6bc54",
+				"sex":"1",
+				"age":29,
+			}],
+		}
+		ws.send(json.dumps(data))
+		result = ws.recv()
+		print result
 	ws.close()
 
 if __name__ == '__main__':
