6dbe5c5bbefd3115267490f586bd27fce2c99ee5
fangzaizai <anmeifang111@sina.com>
2017-10-30 10:44:24 +0800

    1w

--- forms.py
+++ forms.py
@@ -3,5 +3,5 @@ from wtforms import StringField, SubmitField, IntegerField
 from wtforms.validators import Required, Length, NumberRange
 
 class SendForm(Form):
-	time = IntegerField('send times', validators=[Required(), NumberRange(1,90)])
+	time = IntegerField('send times', validators=[Required(), NumberRange(1,50001)])
 	submit = SubmitField('Send')
--- send_face_data.py
+++ send_face_data.py
@@ -40,15 +40,15 @@ def send_face_data():
 	form = SendForm()
 	ws = websocket.create_connection("ws://192.168.9.208:19101/FACE-DETECT-WS/websck")
 	#init parameter
-	face_unid_unique=int(1)
-	face_refid_unique=int(1)
-	event_refid=int(1)
+	face_unid_unique=int(21234)
+	face_refid_unique=int(25678)
+	event_refid=int(21357)
 	#get times
 	#start sending
 	time=session.get('time')
 	print time
 	print type(time)
-	for i in range(1,int(time)):
+	for i in range(19999,19999+int(time)):
 		data={
 		"type":"request",
 		"id":face_unid_unique,
@@ -56,10 +56,10 @@ def send_face_data():
 		"params":[
 			{
 				"face_refid":face_refid_unique,
-				"event_refid":event_refid,
+				"event_refid":"anmeifang",
 				"event_dt":'2017-10-17T08:30:39.028',
 				#"pic_url":"http://192.168.9.208:7788//LINDASceneAlarm/20171017/16/" + str(picname+1) + ".jpg",
-				"pic_url":"http://192.168.9.208:7788//LINDASceneAlarm/20171017/16/{DF452E44-B458-446D-9273-35472618DE73}-20171017164149674-00.jpg",
+				"pic_url":"http://192.168.9.208:7788//LINDASceneAlarm/20171017/16/"+ str(i) +".jpg",
 				"accord_event_refid":"DF452E44-B458-446D-9273-35472618DE73}-20171011160011826",
 				"accord_similarity":90,
 				"camera_refid":"0557fc08-565d-449d-82b9-22ad08e6bc54",
@@ -77,6 +77,7 @@ def send_face_data():
 		face_refid_unique=face_refid_unique+1
 		event_refid=event_refid+1
 	ws.close()
+	print "send over"
 	return render_template('index.html', form=form)
 
 #except socket.error
